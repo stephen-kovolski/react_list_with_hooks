ef709a5d36ea3e4cc6de8dbac054a2556d239c50
stephen-kovolski <s.kovolski@gmail.com>
2020-10-01 22:41:11 -0400

    complete

--- practice_w_hooks/src/components/NewSongForm.js
+++ practice_w_hooks/src/components/NewSongForm.js
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
 
-const NewSongForm = ({addSong}) => {
-    const [title, setTitle] = useState('');
-    const handleSubmit = (e) =>  {
-        e.preventDefault();
-        addSong(title);
-        setTitle('');
-    }
-    return ( 
-        <form onSubmit={handleSubmit}>
-            <label>Song Name:</label>
-            <input type='text' value={title} onChange={(e) => setTitle(e.target.value)}/>
-            <input type='submit' value='add song' />
-        </form>
-     );
+const NewSongForm = ({ addSong }) => {
+  const [title, setTitle] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addSong(title);
+    setTitle('');
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Song name:</label>
+      <input type='text' value={title} onChange={(e) => setTitle(e.target.value)} />
+      <input type='submit' value='add' />
+    </form>
+  );
 }
  
 export default NewSongForm;
